16f55bafcf67a66ac12d212360b75b786a473a65
HARUNA-MORI <harunamori1979@yahoo.co.jp>
2023-09-04 20:51:28 +0900

    Initial Commit

---
+++ TaxApp_02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxApp_02
+//
+//  Created by 春菜森 on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
